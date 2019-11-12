289e0b528ca66ea21f931fadb19e69daa0656d9a
Catherine Callcott-Stevens <ccallcottstevens@gmail.com>
2019-11-13 10:10:21 +1100

    asked a boolean

--- improbability_drive.rb
+++ improbability_drive.rb
@@ -10,4 +10,5 @@
 
 puts "Greetings. I am the Improbability Drive for the Heart of Gold. Please enter your name."
 name = gets.chomp
-puts "I work out the chance of negative repurcusions arising from your decisions. #{name}, are you worried about the effects of a decision you have to make today?" 
+puts "I work out the chance of negative repurcusions arising from your decisions. #{name}, you are worried about the effects of a decision you have to make today? True or false?"
+paranoid = gets.chomp 
