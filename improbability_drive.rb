57dbdcb3fddd613dd6607e2c1d2c53c7c07987cd
Catherine Callcott-Stevens <ccallcottstevens@gmail.com>
2019-11-13 10:01:06 +1100

    used gets

---
+++ improbability_drive.rb
@@ -0,0 +1,12 @@
+####### Terminal Application #######
+
+####### Ruby Gems #######
+
+#require 'colourize'
+
+####### Methods #######
+
+####### Terminal App #######
+
+puts "Greetings. I am the Improbability Drive for the Heart of Gold. Please enter your name."
+name = gets.chomp
