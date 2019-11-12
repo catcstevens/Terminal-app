f91739d8ef58458fc62f86a3a1d6d9ff08439b2a
Catherine Callcott-Stevens <ccallcottstevens@gmail.com>
2019-11-13 10:52:34 +1100

    added boolean

--- improbability_drive.rb
+++ improbability_drive.rb
@@ -10,5 +10,15 @@
 
 puts "Greetings. I am the Improbability Drive for the Heart of Gold. Please enter your name."
 name = gets.chomp
-puts "I work out the chance of negative repurcusions arising from your decisions. #{name}, you are worried about the effects of a decision you have to make today? True or false?"
-paranoid = gets.chomp 
+puts "I work out the chance of negative repurcusions arising from your decisions. #{name}, you are worried about the effects of a decision you have to make today? true or false?"
+paranoid = gets.chomp.downcase
+
+
+if paranoid == "true"
+    puts "what is the decision you are worried about?"
+    decision = gets.chomp
+
+else paranoid == "false"
+    puts "There is no growth without difficult decisions!"
+
+end
