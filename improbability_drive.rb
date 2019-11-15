fe059a754fafe74804d26f98281393d66ada7f24
Catherine Callcott-Stevens <ccallcottstevens@gmail.com>
2019-11-16 09:47:27 +1100

    fixed readability issues

--- improbability_drive.rb
+++ improbability_drive.rb
@@ -5,15 +5,10 @@ require 'faker'
 require 'colorize'
 require 'artii'
 require 'tty-prompt'
-#require 'asciiart'
-
-####### Images #######
-
-# lightspeed_image = AsciiArt.new("./images/approaching.jpg")
-# stars_image = AsciiArt.new("./images/stars.jpg")
 
 ####### Arrays #######
 
+#Array for quotes from HitchhikersGuideToTheGalaxy
 quotes = [
      "'It is a mistake to think you can solve any major problems just with potatoes.' ",
      "'Shee, you guys are so unhip it’s a wonder your bums don’t fall off.' ",
@@ -24,10 +19,13 @@ quotes = [
  ]
  
 # ####### Methods #######
+
+#clears terminal to make it less cluttered
 def clear
     print "\e[H\e[2J"
 end
 
+# loops through still asciiart to animate it
 def animation
     3.times do
     i = 1
@@ -44,24 +42,30 @@ end
 ####### terminal App #######
 begin
 
+    #Asciiart greeting in red
+    a = Artii::Base.new :font => 'big'
+    puts a.asciify("GREETINGS").colorize(:red)
+    puts "I am the Improbability Drive for the Heart of Gold."
 
-a = Artii::Base.new :font => 'big'
-puts a.asciify("GREETINGS").colorize(:red)
-puts "I am the Improbability Drive for the Heart of Gold."
-puts "Please enter your name."
-name = gets.chomp
+    # plays clip of the computer from original radio show
+    `afplay -t 7 ./hithere.mp3`
+
+    puts "Please enter your name."
+    name = gets.chomp
 
     anxiety = true
-        until anxiety == false
-    
-            puts "I work out the chance of negative repurcussions arising from your decisions."
-            puts "#{name}, you are worried about the effects of a decision you have to make today?" 
-            
-        
-            puts "true, false or quit?"
-            paranoid = gets.chomp.downcase
 
-            
+    ########BEGIN OF UNTIL LOOP#######
+    until anxiety == false
+    
+        puts "I work out the chance of negative repurcussions arising from your decisions."
+        puts "#{name}, you are worried about the effects of a decision you have to make today?" 
+        
+    
+        puts "true, false or quit?"
+        paranoid = gets.chomp.downcase
+
+        ######BEGIN OF IF(TRUE) ELSIF(FALSE) ELSIF(TRUE)#####
         if paranoid == "true"
 
             puts "what is the decision you are worried about?"
@@ -74,47 +78,53 @@ name = gets.chomp
             puts
             puts
    
+            #######BEGIN OF IF IMPROBABILITY IS TOO HIGH OR ELSE#######
+            if odds > 100
 
-                if odds > 100
+                puts "The improbability factor is too high".colorize(:red)
+                puts "entering light speed!".colorize(:red)
+                puts
+                sleep(4.0)
+                animation
+                #clear
+                
+            else
+                puts
+                a = Artii::Base.new :font => 'big'
+                puts a.asciify("DO IT").colorize(:red)
+                    #puts artii "You are safe to make your decision."
+                puts
 
-                    puts "The improbability factor is too high".colorize(:red)
-                    puts "entering light speed!".colorize(:red)
-                    puts
-                    sleep(4.0)
-                    animation
-                    #clear
-                    
-                else
-                    puts
-                    a = Artii::Base.new :font => 'big'
-                    puts a.asciify("DO IT").colorize(:red)
-                     #puts artii "You are safe to make your decision."
-                    puts
-
-                end
-
+            end
+            #######END OF IF ELSE#######
             
         elsif paranoid == "false"
             clear
             puts
-        puts quotes.sample
-    #       Faker quote from Hitchhikers Guide to the Galaxy
-     #      puts Faker::Movies::HitchhikersGuideToTheGalaxy.quote.colorize(:red)
 
-        puts
+            #PRINTS RANDOM HITCHHIKERS QUOTE FROM ARRAY#
+            puts quotes.sample
+            puts "The Hitchhiker's Guide to the Galaxy - Douglas Adams"
+          
+            puts
             
         elsif paranoid == "quit"
             anxiety = false
             clear
+
+            #PRINTS DON'T PANIC IN RED ASCIIART#
             a = Artii::Base.new :font => 'big'
-puts a.asciify("Don't Panic").colorize(:red)
+            puts a.asciify("Don't Panic").colorize(:red)
+
+            #VOICE SAYS DON'T PANIC#
             `say "Don't Panic"`
             exit 
         end
+        #######END OF ELSE ELSIF ELSIF########
+    end
+    #######END OF LOOP#######
 
-end
-`say "Don't Panic"`
-
+    #######IF THERE IS A BUG THIS WILL HAPPEN#######
 rescue
     puts "Don't Panic! quit to end program."
     `say "Don't Panic"`
